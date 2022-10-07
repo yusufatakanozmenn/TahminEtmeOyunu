9e48cd18af741e5fb4c35ad899888f9662580108
Yusuf Atakan Özmen <2200201030@ozal.edu.tr>
2022-10-07 17:48:24 +0300

    Proje bitti

--- TahminEtmeOyunu/Program.cs
+++ TahminEtmeOyunu/Program.cs
@@ -2,6 +2,34 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string secretWord = "giraffe";
+        string guess = "";
+        int guessCount = 0;
+        int guessLimit = 3;
+        bool outOfGuesses = false;
+        while (guess != secretWord && !outOfGuesses)
+        {
+            if (guessCount < guessLimit)
+            {
+                Console.Write("Tahmininizi giriniz: ");
+                guess = Console.ReadLine();
+                guessCount++;
+            }
+            else
+            {
+                outOfGuesses = true;
+            }
+        }
+        if (outOfGuesses)
+        {
+            Console.WriteLine("Kaybettiniz!");
+        }
+        else
+        {
+            Console.WriteLine("Tebrikler, Kazandınız!");
+        }   
+        
+
+
     }
 }
